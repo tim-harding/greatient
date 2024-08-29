3faaa39a023afe93b29e6ba2e3d0b7f51a718d28
Tim Harding <Tim@TimHarding.co>
2024-08-29 00:21:14 -0700

    Documentation comments for shared functions

--- conversion/shared.js
+++ conversion/shared.js
@@ -1,11 +1,29 @@
+/**
+ * Equivalent to `n % m`, except that negative numbers are turned positive.
+ * @param {number} n - Number to take the modulus of
+ * @param {number} m - Modulus
+ * @returns {number}
+ */
 export function fmod(n, m) {
   return ((n % m) + m) % m;
 }
 
+/**
+ * Constrains a value to the given range
+ * @param {number} n - Value to constrain
+ * @param {number} min - Lower bound
+ * @param {number} max - Upper bound
+ * @returns {number}
+ */
 export function clamp(n, min, max) {
   return Math.min(max, Math.max(min, n));
 }
 
+/**
+ * Constrains a value within the range 0..1
+ * @param {number} n
+ * @returns {number}
+ */
 export function clamp01(n) {
   return clamp(n, 0, 1);
 }
