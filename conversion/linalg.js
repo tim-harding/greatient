397118b719c5a25affa97927dd2a8dae35b2753c
Tim Harding <Tim@TimHarding.co>
2024-08-29 13:36:08 -0700

    Add constructors for linalg types

--- conversion/linalg.js
+++ conversion/linalg.js
@@ -13,13 +13,50 @@
  */
 
 /**
- * Multiply two column-major 3×3 matrices
+ * Create a new {@link Vector}
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @return {Vector}
+ */
+export function Vector(x, y, z) {
+  return [x, y, z];
+}
+
+/**
+ * Create a new {@link Matrix}
+ * @param {number} m00
+ * @param {number} m01
+ * @param {number} m02
+ * @param {number} m10
+ * @param {number} m11
+ * @param {number} m12
+ * @param {number} m20
+ * @param {number} m21
+ * @param {number} m22
+ * @return {Matrix}
+ */
+// prettier-ignore
+export function Matrix(
+  m00, m01, m02, 
+  m10, m11, m12, 
+  m20, m21, m22,
+) {
+  return [
+    m00, m01, m02, 
+    m10, m11, m12, 
+    m20, m21, m22
+  ];
+}
+
+/**
+ * Multiply two {@link Matrix matrices}
  * @param {Matrix} a
  * @param {Matrix} b
  * @returns {Matrix}
  */
 export function mulmat(a, b) {
-  const o = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const o = Matrix(0, 0, 0, 0, 0, 0, 0, 0, 0);
   for (let y = 0; y < 3; y++) {
     for (let x = 0; x < 3; x++) {
       const i = y * 3 + x;
@@ -32,13 +69,13 @@ export function mulmat(a, b) {
 }
 
 /**
- * Multiply a column-major 3×3 matrix with a 3D vector
+ * Multiply a {@link Matrix} with a {@link Vector}
  * @param {Matrix} m
  * @param {Vector} v
  * @returns {Vector}
  */
 export function mulvec(m, v) {
-  const o = [0, 0, 0];
+  const o = Vector(0, 0, 0);
   for (let y = 0; y < 3; y++) {
     for (let x = 0; x < 3; x++) {
       o[y] += v[x] * m[y * 3 + x];
