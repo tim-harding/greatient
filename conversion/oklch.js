c50d75f6b1bf9989c67f31f03c74854e8c4298b4
Tim Harding <Tim@TimHarding.co>
2024-08-29 12:46:58 -0700

    Extracted oklch

--- conversion/oklch.js
+++ conversion/oklch.js
@@ -1,10 +1,10 @@
-export function oklabToOklch(oklab) {
+export function fromOklab(oklab) {
   const [l, a, b] = oklab;
   var hue = (Math.atan2(b, a) * 180) / Math.PI;
   return [l, Math.sqrt(a ** 2 + b ** 2), hue >= 0 ? hue : hue + 360];
 }
 
-export function oklchToOklab(oklch) {
+export function toOklab(oklch) {
   const [l, c, h] = oklch;
   return [
     l,
