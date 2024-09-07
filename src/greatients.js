34b31c8985dac4f8005311d7297bfb16262c713f
Tim Harding <Tim@TimHarding.co>
2024-09-06 21:36:50 -0700

    Use standalone functions for API
    Better tree shaking

--- src/greatients.js
+++ src/greatients.js
@@ -2,143 +2,139 @@
 
 import { SHADER_VERTEX, SHADER_FRAGMENT } from "./shader.js";
 
-export class Greatient {
-  #boundingClientRect = { width: 0, height: 0 };
-  /** @type {HTMLCanvasElement} */
-  #canvas;
-  /** @type {WebGL2RenderingContext} */
-  #gl;
-  /** @type {WebGLProgram} */
-  #program;
-  /** @type {WebGLVertexArrayObject} */
-  #vao;
-  /** @type {GLint} */
-  #attributePosition;
+/**
+ * @typedef {Object} Size
+ * @property {number} width
+ * @property {number} height
+ */
+
+/**
+ * @typedef {Object} Greatient
+ * @property {Size} boundingClientRect
+ * @property {WebGL2RenderingContext} gl
+ * @property {HTMLCanvasElement} canvas
+ * @property {WebGLProgram} program
+ * @property {WebGLVertexArrayObject} vao
+ * @property {GLint} attributePosition
+ */
+
+/**
+ * @param {HTMLCanvasElement} canvas
+ */
+export function Greatient(canvas) {
+  const gl = canvas.getContext("webgl2", {
+    alpha: false,
+    premultipliedAlpha: true,
+    depth: true,
+    antialias: true,
+    preserveDrawingBuffer: true,
+    powerPreference: "low-power",
+  });
+  if (!gl) throw new Error("WebGL2 not available");
+
+  if (window.matchMedia("(color-gamut: p3)").matches) {
+    gl.drawingBufferColorSpace = "display-p3";
+  }
+  gl.enable(gl.DEPTH_TEST);
+  gl.enable(gl.CULL_FACE);
+
+  const vertexShader = createShader(gl, gl.VERTEX_SHADER, SHADER_VERTEX);
+  const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, SHADER_FRAGMENT);
+  const program = createProgram(gl, vertexShader, fragmentShader);
+
+  const attributePosition = gl.getAttribLocation(program, "a_position");
+
+  const positionBuffer = gl.createBuffer();
+  if (!positionBuffer) throw glError(gl);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  const positions = [
+    -0.5, -0.5, 0.5, -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5,
+  ];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+  const vao = gl.createVertexArray();
+  if (!vao) throw glError(gl);
+
+  const out = {
+    gl,
+    canvas,
+    program,
+    vao,
+    attributePosition,
+    boundingClientRect: { width: 0, height: 0 },
+  };
 
   /**
-   * @param {HTMLCanvasElement} canvas
+   * @param {number} width
+   * @param {number} height
    */
-  constructor(canvas) {
-    this.#canvas = canvas;
-
-    const gl = canvas.getContext("webgl2", {
-      alpha: false,
-      premultipliedAlpha: true,
-      depth: true,
-      antialias: true,
-      preserveDrawingBuffer: true,
-      powerPreference: "low-power",
+  const handleResizeShared = (width, height) => {
+    canvas.width = width;
+    canvas.height = height;
+    gl.viewport(0, 0, width, height);
+    redrawImmediate(out);
+  };
+  let resize = new ResizeObserver((entries) => {
+    const size = entries[0].devicePixelContentBoxSize[0];
+    handleResizeShared(size.inlineSize, size.blockSize);
+  });
+  try {
+    resize.observe(canvas, { box: "device-pixel-content-box" });
+  } catch (e) {
+    // Safari still doesn't support device-pixel-content-box :/
+    resize = new ResizeObserver((entries) => {
+      let { inlineSize: width, blockSize: height } =
+        entries[0].contentBoxSize[0];
+      const dpr = window.devicePixelRatio;
+      handleResizeShared(width * dpr, height * dpr);
     });
-    if (!gl) throw new Error("WebGL2 not available");
-    this.#gl = gl;
-
-    if (window.matchMedia("(color-gamut: p3)").matches) {
-      gl.drawingBufferColorSpace = "display-p3";
-    }
-    gl.enable(gl.DEPTH_TEST);
-    gl.enable(gl.CULL_FACE);
-
-    const vertexShader = createShader(gl, gl.VERTEX_SHADER, SHADER_VERTEX);
-    const fragmentShader = createShader(
-      gl,
-      gl.FRAGMENT_SHADER,
-      SHADER_FRAGMENT,
-    );
-
-    const program = createProgram(gl, vertexShader, fragmentShader);
-    this.#program = program;
-
-    this.#attributePosition = gl.getAttribLocation(program, "a_position");
-
-    const positionBuffer = gl.createBuffer();
-    if (!positionBuffer) throw glError(gl);
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    const positions = [
-      -0.5, -0.5, 0.5, -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5,
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-    const vao = gl.createVertexArray();
-    if (!vao) throw glError(gl);
-    this.#vao = vao;
-
-    /**
-     * @param {number} width
-     * @param {number} height
-     */
-    const handleResizeShared = (width, height) => {
-      canvas.width = width;
-      canvas.height = height;
-      gl.viewport(0, 0, width, height);
-      this._redrawImmediate();
-    };
-    let resize = new ResizeObserver((entries) => {
-      const size = entries[0].devicePixelContentBoxSize[0];
-      handleResizeShared(size.inlineSize, size.blockSize);
-    });
-    try {
-      resize.observe(canvas, { box: "device-pixel-content-box" });
-    } catch (e) {
-      // Safari still doesn't support device-pixel-content-box :/
-      resize = new ResizeObserver((entries) => {
-        let { inlineSize: width, blockSize: height } =
-          entries[0].contentBoxSize[0];
-        const dpr = window.devicePixelRatio;
-        handleResizeShared(width * dpr, height * dpr);
-      });
-      resize.observe(canvas, { box: "content-box" });
-    }
+    resize.observe(canvas, { box: "content-box" });
   }
 
-  /**
-   * @param { "bezier" | "catmull-rom" | "bspline" } mode
-   */
-  setInterpolation(mode) {}
+  return out;
+}
 
-  /**
-   * This callback is called to modify the bezier points of a {@link Greatient}.
-   * @callback updateBezierPointsCallback
-   * @param {Float32Array} points
-   */
+/**
+ * This callback is called to modify the bezier points of a {@link Greatient}.
+ * @callback updateBezierPointsCallback
+ * @param {Float32Array} points
+ */
 
-  /**
-   * Update the set of
-   * @param {updateBezierPointsCallback} callback
-   */
-  updateBezierPoints(callback) {}
+/**
+ * Update the set of
+ * @param {updateBezierPointsCallback} callback
+ */
+function updateBezierPoints(callback) {}
 
-  /**
-   * Repaint the canvas. When manually driving gradient animations, this should
-   * be called in a `requestAnimationFrame` callback.
-   */
-  redraw() {
-    const rect = this.#canvas.getBoundingClientRect();
-    const rectPrev = this.#boundingClientRect;
-    if (rect.width != rectPrev.width || rect.height != rectPrev.height) {
-      // Handle drawing in the ResizeObserver callback instead.
-      this.#boundingClientRect = rect;
-      return;
-    }
-    this._redrawImmediate();
+/**
+ * Repaint the canvas. When manually driving gradient animations, this should
+ * be called in a `requestAnimationFrame` callback.
+ * @param {Greatient} greatient
+ */
+function redraw(greatient) {
+  const rect = greatient.canvas.getBoundingClientRect();
+  const rectPrev = greatient.boundingClientRect;
+  if (rect.width != rectPrev.width || rect.height != rectPrev.height) {
+    // Handle drawing in the ResizeObserver callback instead.
+    greatient.boundingClientRect = rect;
+    return;
   }
+  redrawImmediate(greatient);
+}
 
-  /**
-   * Execute the WebGL canvas drawing commands.
-   * @private
-   */
-  _redrawImmediate() {
-    const gl = this.#gl;
-    const program = this.#program;
-    const posAttr = this.#attributePosition;
-
-    gl.clearColor(0, 0.2, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.useProgram(program);
-    gl.bindVertexArray(this.#vao);
-    gl.enableVertexAttribArray(posAttr);
-    gl.vertexAttribPointer(posAttr, 2, gl.FLOAT, false, 0, 0);
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
-  }
+/**
+ * Execute the WebGL canvas drawing commands.
+ * @param {Greatient} greatient
+ */
+function redrawImmediate(greatient) {
+  const { gl, program, vao, attributePosition } = greatient;
+  gl.clearColor(0, 0.2, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.useProgram(program);
+  gl.bindVertexArray(vao);
+  gl.enableVertexAttribArray(attributePosition);
+  gl.vertexAttribPointer(attributePosition, 2, gl.FLOAT, false, 0, 0);
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
 }
 
 /**
