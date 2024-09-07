e63db51fa88f11acf784f28469740783e9673343
Tim Harding <Tim@TimHarding.co>
2024-09-07 10:30:46 -0700

    Redesigned bezier data structure

--- src/bezier/index.js
+++ src/bezier/index.js
@@ -1,95 +1,49 @@
 /**
- * Create a new data structure for the control points of a Bezier control mesh
- * @param {number} width
- * @param {number} height
+ * Create a array containing the control points of a bezier control mesh.
+ * Each control point is specified by ten numbers:
+ *
+ * - Anchor (x,y)
+ * - Top control point (x,y)
+ * - Bottom control point (x,y)
+ * - Left control point (x,y)
+ * - Right control point (x,y)
+ *
+ * The control are stored one after another, row by row.
+ *
+ * @param {number} width Number of control points in the x-axis
+ * @param {number} height Number of control points in the y-axis
  * @return {Float32Array}
  */
 export function createControlPoints(width, height) {
-  const x = width * 3 + 1;
-  const y = height * 3 + 1;
-  return new Float32Array(x * y);
+  return new Float32Array(width * height * 10);
 }
 
 /**
- * Get the index of the anchor position for the given control point.
- * @param {number} x x-index of the control point
- * @param {number} y y-index of the control point
- * @param {number} w Width of the control mesh
- * @param {number} h Height of the control mesh
- * @return {number} Anchor point index or -1 if not found
+ * Resize a bezier control mesh previously created with {@see createControlPoints}.
+ * @param {Float32Array} oldControlPoints
+ * @param {number} oldWidth Current number of control points in the x-axis
+ * @param {number} oldHeight Current number of control points in the y-axis
+ * @param {number} newWidth Number of control points in the x-axis to resize to
+ * @param {number} newHeight Number of control points in the y-axis to resize to
  */
-export function anchor(x, y, w, h) {
-  return isValidArgument(x, y, w, h) ? y * 3 * w + x * 3 : -1;
-}
-
-/**
- * Get the index of the anchor position for the given control point.
- * @param {number} x x-index of the control point
- * @param {number} y y-index of the control point
- * @param {number} w Width of the control mesh
- * @param {number} h Height of the control mesh
- * @return {number} Anchor point index or -1 if not found
- */
-export function handleWest(x, y, w, h) {
-  return isValidArgument(x, y, w, h) && x > 0 ? y * 3 * w + x * 3 - 1 : -1;
-}
-
-/**
- * Get the index of the anchor position for the given control point.
- * @param {number} x x-index of the control point
- * @param {number} y y-index of the control point
- * @param {number} w Width of the control mesh
- * @param {number} h Height of the control mesh
- * @return {number} Anchor point index or -1 if not found
- */
-export function handleEast(x, y, w, h) {
-  return isValidArgument(x, y, w, h) && x < w - 1 ? y * 3 * w + x * 3 + 1 : -1;
-}
-
-/**
- * Get the index of the anchor position for the given control point.
- * @param {number} x x-index of the control point
- * @param {number} y y-index of the control point
- * @param {number} w Width of the control mesh
- * @param {number} h Height of the control mesh
- * @return {number} Anchor point index or -1 if not found
- */
-export function handleNorth(x, y, w, h) {
-  return isValidArgument(x, y, w, h) && y > 0 ? (y - 1) * 3 * w + x * 3 : -1;
-}
-
-/**
- * Get the index of the anchor position for the given control point.
- * @param {number} x x-index of the control point
- * @param {number} y y-index of the control point
- * @param {number} w Width of the control mesh
- * @param {number} h Height of the control mesh
- * @return {number} Anchor point index or -1 if not found
- */
-export function handleNorth(x, y, w, h) {
-  return isValidArgument(x, y, w, h) && y > 0 ? (y - 1) * 3 * w + x * 3 : -1;
-}
-
-/**
- * Checks whether the arguments to one of the control point getter functions are
- * valid.
- * @param {number} x x-index of the control point
- * @param {number} y y-index of the control point
- * @param {number} w Width of the control mesh
- * @param {number} h Height of the control mesh
- * @return {boolean} Whether the given set of arguments is valid
- */
-function isValidArgument(x, y, w, h) {
-  return (
-    Number.isInteger(x) &&
-    Number.isInteger(y) &&
-    Number.isInteger(w) &&
-    Number.isInteger(h) &&
-    w > 0 &&
-    h > 0 &&
-    x >= 0 &&
-    x < w &&
-    y >= 0 &&
-    y < h
-  );
+export function resizeControlPoints(
+  oldControlPoints,
+  oldWidth,
+  oldHeight,
+  newWidth,
+  newHeight,
+) {
+  const out = createControlPoints(newWidth, newHeight);
+  const w = Math.min(oldWidth, newWidth);
+  const h = Math.min(oldHeight, newHeight);
+  for (let y = 0; y < h; y++) {
+    for (let x = 0; x < w; x++) {
+      const srcStart = y * oldWidth + x;
+      const srcEnd = srcStart + w;
+      const dstStart = y * newWidth + x;
+      const src = oldControlPoints.subarray(srcStart * 10, srcEnd * 10);
+      out.set(src, dstStart * 10);
+    }
+  }
+  return out;
 }
